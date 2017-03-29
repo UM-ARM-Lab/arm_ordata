16e566adff299bd7cb3ffafdae09b46aaabb135a
Dale McConachie <dmcconac@umich.edu>
2017-03-29 18:40:48 -0400

    Sample Wiktor python file.

--- scripts/dual_arm_urdf_srdf_test.py
+++ scripts/dual_arm_urdf_srdf_test.py
@@ -8,14 +8,14 @@ import math
 import numpy as np
 
 
-def waitrobot(robot):
-    """busy wait for robot completion"""
-    while not robot.GetController().IsDone():
-        time.sleep(0.01)
-
-
 if __name__ == "__main__":
+    rave.misc.InitOpenRAVELogging()
     env = rave.Environment()  # create openrave environment
+    # rave.RaveSetDebugLevel(rave.DebugLevel.Debug)  # set output level to debug
+
+    collision_checker = rave.RaveCreateCollisionChecker(env, 'ode')
+    env.SetCollisionChecker(collision_checker)
+
     # attach viewer (optional)
     env.SetViewer('qtcoin')
     # env.SetViewer('InteractiveMarker')
@@ -27,14 +27,39 @@ if __name__ == "__main__":
         [ 0.,  0.,  0.,  1.]
     ])
 
-    # rospack = rospkg.RosPack() # Get a object that can find file paths to packages in ROS
-    # urdf_package_path = rospack.get_path('arm_ordata')
-    # srdf_package_path = rospack.get_path('dual_arm_moveit_config')
-
     urdf_module = rave.RaveCreateModule(env, 'urdf')
 
     with env:
         name = urdf_module.SendCommand('LoadURI package://wiktor_description/urdf/wiktor.urdf package://wiktor_moveit_config/config/wiktor.srdf')
         robot = env.GetRobot(name)
+
+    robot.SetActiveManipulator('left_arm')
+    manip = robot.GetActiveManipulator()
+
+    ikmodel = rave.databases.inversekinematics.InverseKinematicsModel(robot, iktype=rave.IkParameterizationType.Transform6D)
+
+    if not ikmodel.load():
+        ikmodel.autogenerate()
+
+    with env:
+        robot.SetActiveDOFs(manip.GetArmIndices())
+        goal_pose = np.array([[1, 0, 0, 0.8],
+                              [0, 1, 0, 0],
+                              [0, 0, 1, 1.2],
+                              [0, 0, 0, 1]])
+
+        # IPython.embed()
+        target_config = manip.FindIKSolution(goal_pose, rave.IkFilterOptions.CheckEnvCollisions)
+        manip_problem = rave.interfaces.BaseManipulation(robot)
+        arm_traj = manip_problem.MoveManipulator(goal = target_config, execute = False, outputtrajobj = True)
+        rave.planningutils.RetimeActiveDOFTrajectory(arm_traj, robot, hastimestamps = False, maxvelmult = 0.1, maxaccelmult = 0.1, plannername = 'ParabolicTrajectoryRetimer')
+
+
+    time.sleep(1)
+
+    robot.GetController().SetPath(arm_traj)
+    robot.WaitForController(0)
+
+    print "Final pose\n", manip.GetEndEffectorTransform()
 
     IPython.embed()
